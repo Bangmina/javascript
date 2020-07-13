93710d8fab3440f04af411634e4b83b8a11281d8
mina4231 <mina94_02@naver.com>
2020-07-13 13:11:08 +0900

    velopert - 객체 : 객체 안에 함수 넣기

--- velopert/object.js
+++ velopert/object.js
@@ -1,8 +1,8 @@
 'use strict';
-// 하나의 이름에 여러가지 값
-const dog = {
-    key : '키값',
-}
+// 1. 객체 : 하나의 이름에 여러가지 값
+// const dog = {
+//     key : '키값',
+// }
 //
 const ironMan = {
     name : '토니 스타크',
@@ -16,7 +16,7 @@ const captainAmerica = {
     alias : '캡틴 아메리카'
 };
 
-// 비구조화할당(객체구조분해) : 특정 값들을 객체에서 빼온다
+// 2. 객체 - 비구조화할당(객체구조분해) : 특정 값들을 객체에서 빼온다
 
 const {name} = ironMan;
 console.log(name); // 토니스타크
@@ -33,3 +33,28 @@ print(captainAmerica);
 
 console.log(ironMan);
 console.log(captainAmerica);
+
+// 3. 객체 -  객체 안에 함수 넣기
+// 함수에 있는 this는 자신이 속해 있는 곳을 가르키는것
+// 화살표함수 내부에서는 this를 인식하지 못한다.
+
+const dog = {
+    name: '멍멍이',
+    sound: '멍멍',
+    say: function() {
+        console.log(this);
+        console.log(this.sound);
+    }
+}
+
+const cat = {
+    name : '야옹이',
+    sound : '야옹~'
+}
+
+cat.say = dog.say;
+dog.say(); // 멍멍
+cat.say(); // 야옹
+
+const catSay = cat.say;
+catSay(); // undefined
