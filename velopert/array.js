06d98a30337367b96021c17b87e54bec3c07a702
mina4231 <mina94_02@naver.com>
2020-07-13 16:13:42 +0900

    velopert - 배열: push length

---
+++ velopert/array.js
@@ -0,0 +1,15 @@
+'use strict';
+// 배열
+const object = [
+    {name:' 멍멍이'},
+    {name: '야옹이'}
+];
+
+console.log(object.length)
+
+// push 배열에 새로운 항목 추가
+object.push({
+    name: '멍뭉이'
+})
+
+console.log(object.length)
