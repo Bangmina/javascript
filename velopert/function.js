617bd46c5650654c00eca936d092924bdd3cc351
mina4231 <mina94_02@naver.com>
2020-07-13 11:53:27 +0900

    velopert - function if,else if,return

--- velopert/function.js
+++ velopert/function.js
@@ -1,17 +1,24 @@
-<<<<<<< HEAD
 // return을 사용하면 함수가 끝난다
 // return 뒤에는 실행되지 않는다.
-=======
->>>>>>> 1b67cff1d8258a8db4f3bde1fcb4c22675b9cb92
-function add(a,b) {
-    return a + b;
+function getGrade(score) {
+    if (score === 100) {
+        return 'A+'
+    } else if(score >= 90) {
+        return 'A';
+    } else if (score ===89 ) {
+        return 'B+';
+    } else if (score >= 80) {
+        return 'B';
+    } else if ( score === 79) {
+        return 'C+'
+    } else if (score >= 70) {
+        return 'C';
+    } else if (score === 69) {
+        return 'D'
+    } else {
+        return 'F'
+    }
 }
 
-const sum = add(1,2);
-console.log(sum);
-
-function hello(name) {
-    console.log(`hello ${name} !`)
-}
-
-hello('velopert')
+const grade = getGrade(69);
+console.log(grade);
