32002ac3be37e87b789677aa42115c96c3ec6089
mina4231 <mina94_02@naver.com>
2020-07-16 17:53:54 +0900

    vlopert - array (forEach)

--- velopert/breakContinue.js
+++ velopert/breakContinue.js
@@ -10,3 +10,16 @@ for (let i= 0; i < 10; i++ ) {
     console.log(i)
     if(i ===5) break;
 }
+
+// Quiz
+
+function sumOf (numbers) {
+    let sum = 0;
+    for (let i =0; i <numbers.length; i++) {
+        sum += numbers[i]
+    }
+    return sum;
+} 
+
+const result = sumOf([1, 2, 3, 4, 5]);
+console.log(result);
---
+++ velopert/forEach.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const superheroes = [
+    '아이언맨',
+    '캡틴아메리카',
+    '토르',
+    '닥터스트레인지'
+]
+//
+function print(hero) {
+    console.log(hero)
+}
+superheroes.forEach(print);
+//
+superheroes.forEach(function(hero){
+    console.log(hero)
+})
+superheroes.forEach(hero => {
+    console.log(hero)
+} )
