30b72e31ddf51f747476eb50252f69a41eefbdf1
mina4231 <mina94_02@naver.com>
2020-07-20 13:44:50 +0900

    velopert - ClassReview

---
+++ velopert/prototypeClassReview.js
@@ -0,0 +1,27 @@
+'use strict';
+
+class Food {
+    constructor(name) {
+        this.name = name;
+        this.brands = [];
+    }
+
+    // 클래스 내부에 구현하는 함수 method
+    addBrand (brand) {
+        this.brands.push(brand);
+    }
+    print() {
+        console.log(`${this.name} 를/을 파는 음식점들`);
+        console.log(this.brands.join(', '));
+    }
+ }
+
+ const pizza = new Food('피자');
+ pizza.addBrand('피자헛');
+ pizza.addBrand('도미노피자');
+ const chicken = new Food('치킨');
+ chicken.addBrand('굽네치킨');
+ chicken.addBrand('BBQ');
+ 
+ pizza.print();
+ chicken.print();
